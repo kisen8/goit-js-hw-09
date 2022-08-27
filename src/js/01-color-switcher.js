bf94b481ea5629d77d7d05c877e3218a7b117983
Ihor Komar <kisenbenz8@gmail.com>
2022-08-27 22:25:09 +0300

    js-09 task-1(color switchers)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+const DELAY_TIME = 1000;
+
+btnStop.disabled = true;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, DELAY_TIME);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(intervalId);
+});
